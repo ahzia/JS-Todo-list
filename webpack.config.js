458732d33785d28dab6100804a274f11143fba8e
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-07-21 00:09:56 +0430

    showing todo list in html

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/js/index.js',
   devServer: {
     contentBase: './dist',
   },
