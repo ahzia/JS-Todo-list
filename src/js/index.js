f26ccf9c73e01d056801b521708c20f158c2e69a
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-07-22 00:29:44 +0430

    added Checkbox Change Event Listeners

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,17 @@
 import '../css/style.css';
 
 let todos = [{
-  index: 4,
+  index: 2,
   description: 'todo 4',
   completed: true,
 },
 {
-  index: 1,
+  index: 0,
   description: ' todo 1',
   completed: false,
 },
 {
-  index: 3,
+  index: 1,
   description: ' todo 3',
   completed: true,
 },
@@ -27,19 +27,32 @@ function sortList() {
     return 0;
   });
 }
+function updateStatus(index) {
+  todos[index].completed = !(todos[index].completed);
+  updateLocalStorage(true);
+}
+function addCheckboxChangeEventListeners() {
+  const checkboxes = document.querySelectorAll('input[type=checkbox][name=todoCheck]');
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', (event) => {
+      const index = (event.target.id).replace('ch', '');
+      updateStatus(index);
+    });
+  });
+}
 function displayList() {
-  sortList();
   const section = document.getElementById('todos');
   const list = document.createElement('ul');
   list.id = 'list';
   todos.forEach((todo) => {
     const { description, completed, index } = todo;
     const liId = `li${index}`;
+    const checkboxId = `ch${index}`;
     let todoCard = `<li id=${liId} class= "todo" >`;
     if (completed) {
-      todoCard += '<input type="checkbox" checked>';
+      todoCard += `<input type="checkbox" name="todoCheck" checked id="${checkboxId}">`;
     } else {
-      todoCard += '<input type="checkbox">';
+      todoCard += `<input type="checkbox" name="todoCheck" id="${checkboxId}">`;
     }
     todoCard += `<p>${description}</p>
     </li>
@@ -48,20 +61,25 @@ function displayList() {
   });
   section.innerHTML = '';
   section.appendChild(list);
+  addCheckboxChangeEventListeners();
 }
-function updateLocalStorage(update) {
+function updateLocalStorage(update, sort) {
   const listFromStorage = window.localStorage.getItem('todos');
-  sortList();
   if (update) {
     // update local storage
+    if (sort) {
+      sortList();
+    }
     window.localStorage.setItem('todos', JSON.stringify(todos));
   } else if (listFromStorage != null) {
     // get data from local storage
     todos = JSON.parse(window.localStorage.getItem('todos'));
   } else {
     // inialize local storage
+    sortList();
     window.localStorage.setItem('todos', JSON.stringify(todos));
   }
   displayList();
 }
+
 updateLocalStorage();
